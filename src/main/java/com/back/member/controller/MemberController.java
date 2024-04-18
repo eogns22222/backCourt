b8a62afbd13ecd6978de8829d1b7ed061086697c
kang_Dae_Hoon <eogns22222@naver.com>
2024-04-18 09:30:02 +0900

    접속 확인

--- src/main/java/com/back/member/controller/MemberController.java
+++ src/main/java/com/back/member/controller/MemberController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.back.member.service.MemberService;
 
@@ -12,5 +13,12 @@ public class MemberController {
 
 	Logger logger = LoggerFactory.getLogger(getClass());
 	@Autowired MemberService memberService;
+	
+	
+	@RequestMapping(value = "/")
+	public String home() {
+		logger.info("접속");
+		return "index";
+	}
 	
 }
