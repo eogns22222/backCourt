faabcf103f1fe47fdfa10052fd29f6c7d6799e41
park_chae_hyeon <apg0210@icloud.com>
2024-04-22 22:56:27 +0900

    코트 디테일 스와이프 완료, 코트 리스트 로직 수정

--- src/main/java/com/back/court/service/CourtService.java
+++ src/main/java/com/back/court/service/CourtService.java
@@ -1,8 +1,12 @@
 package com.back.court.service;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,29 +23,82 @@ public class CourtService {
 	@Autowired
 	CourtDAO courtDAO;
 
-	public Map<String, Object> list(int page, String address) {
+	public Map<String, Object> list(int page, String address, String searchCategory, String searchWord,
+			String searchFlag) {
 
-		Map<String, Object> result = new HashMap<String, Object>();
-		List<CourtDTO> list;
-		List<CourtDTO> allList = courtDAO.allList();
-
+		// 시작 페이지
 		int start = (page - 1) * 10;
 
-		if (address.equals("") == true) {
-//			logger.info("servcie.list / address = {} /", address);
-			list = courtDAO.list(start);
-			result.put("totalPage", courtDAO.allCourtCount());
+		Map<String, Object> map = new HashMap<String, Object>();
 
-		} else {
-//			logger.info("servcie.list / address = {} /", address);
-			list = courtDAO.listFilterAddress(start, address);
-			result.put("totalPage", courtDAO.addressFilteringCourtCount(address));
+		// 모든 리스트
+		List<CourtDTO> list = courtDAO.list();
+		// 가공될 리스트
+		List<CourtDTO> processedList = new ArrayList<CourtDTO>();
+		
+		// 주소 리스트
+		map.put("addressList", addressDeduplication(list));
 
+		// 지역명 검색
+		if (searchFlag.equals("true") && searchCategory.equals("courtAddress")) { //지역명 검색
+			processedList = addressProcessedListing(list, searchWord);
+		} else if (searchFlag.equals("true") && searchCategory.equals("courtName")) {//구장명 검색
+			processedList = nameProcessedListing(list, searchWord);
+		} else if (address.isEmpty() == false) { // 주소필터
+			processedList = addressProcessedListing(list, address);
+		} else { // 기본
+			processedList = list;
 		}
 
-		result.put("list", list);
-		result.put("allList", allList);
-		return result;
+		// 리스트에 추가
+		map.put("list", pagingList(processedList, start));
+		
+		// 총 페이지
+		map.put("totalPage", processedList.size());
+
+		return map;
+	}
+	// 주소 구 단위로 자르고 가나다순 정렬
+	public List<String> addressDeduplication(List<CourtDTO> list) {
+		Set<String> set = new HashSet<String>();
+		
+		for (CourtDTO dto : list) {
+			set.add(dto.getCourtAddress().split(" ")[1]);
+		}
+		List<String> DeduplicationList = new ArrayList<String>(set);
+		Collections.sort(DeduplicationList);
+		return DeduplicationList;
+	}
+
+	// 구장명 필터 메서드
+	public List<CourtDTO> nameProcessedListing(List<CourtDTO> list, String courtName) {
+		List<CourtDTO> processedList = new ArrayList<CourtDTO>();
+		for (CourtDTO dto : list) {
+			if (dto.getCourtName().contains(courtName)) {
+				processedList.add(dto);
+			}
+		}
+		return processedList;
+	}
+
+	// 주소 필터 메서드
+	public List<CourtDTO> addressProcessedListing(List<CourtDTO> list, String address) {
+		List<CourtDTO> processedList = new ArrayList<CourtDTO>();
+		for (CourtDTO dto : list) {
+			if (dto.getCourtAddress().contains(address)) {
+				processedList.add(dto);
+			}
+		}
+		return processedList;
+	}
+
+	// 페이징 처리위해 10개씩만 
+	public List<CourtDTO> pagingList(List<CourtDTO> processedList, int start) {
+		List<CourtDTO> list = new ArrayList<CourtDTO>();
+		for (int i = start; i < Math.min(start + 10, processedList.size()); i++) {
+			list.add(processedList.get(i));
+		}
+		return list;
 	}
 
 	public boolean jjim(String id, int courtIdx) {
@@ -62,57 +119,18 @@ public class CourtService {
 		return true;
 	}
 
-	public Map<String, Object> searchList(String courtSearchCategory, String courtSearchWord, int page,String address) {
-
-		Map<String, Object> result = new HashMap<String, Object>();
-		List<CourtDTO> list;
-		List<CourtDTO> allList = courtDAO.allList();
-		
-		int start = (page - 1) * 10;
-		
-		if(courtSearchCategory.equals("courtAddress")) {
-			list = courtDAO.addressSearchList(courtSearchWord,start);
-			logger.info("서치리스트 코트어드레스 ");
-			result.put("list", list);
-			result.put("totalPage", courtDAO.addressFilteringCourtCount(courtSearchWord));
-		}else if(courtSearchCategory.equals("courtName")) {
-			list = courtDAO.nameSearchList(courtSearchWord,start);
-			logger.info("서치리스트 코트네임 ");
-			result.put("list", list);
-			result.put("totalPage", courtDAO.nameFilteringCourtCount(courtSearchWord));
-		}
-		
-//		if (address.equals("") == true) {
-////			logger.info("servcie.list / address = {} /", address);
-//			list = courtDAO.SearchList(courtSearchCategory,courtSearchWord,start);
-//			result.put("totalPage", courtDAO.allCourtCount());
-//
-//		} else {
-////			logger.info("servcie.list / address = {} /", address);
-//			list = courtDAO.listFilterAddress(start, address);
-//			result.put("totalPage", courtDAO.addressFilteringCourtCount(address));
-//
-//		}
-		
-		result.put("allList", allList);
-		return result;
-	}
-
-	public Map<String, Object> detail(String courtIdx) {
+	public Map<String, Object> detail(String courtIdx, String selectDate) {
 		Map<String, Object> map = new HashMap<String, Object>();
-		
+
 		List<CourtDTO> detail = courtDAO.detail(courtIdx);
 		List<String> fileName = courtDAO.fileNameList(courtIdx);
-		List<String> bookingStartTime = courtDAO.bookingStartTimeList(courtIdx);
-		
-		
-		
+		List<String> bookingStartTime = courtDAO.bookingStartTimeList(courtIdx, selectDate);
+
 		map.put("detail", detail);
 		map.put("fileName", fileName);
 		map.put("bookingStartTime", bookingStartTime);
-		
+
 		return map;
 	}
-
 
 }
