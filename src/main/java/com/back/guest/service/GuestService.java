f86e95474fe30d705a766b77a51ee8da13132931
song_seung_min <tmdals9781@naver.com>
2024-04-24 16:57:17 +0900

    게스트 모집글 수정페이지 완료

--- src/main/java/com/back/guest/service/GuestService.java
+++ src/main/java/com/back/guest/service/GuestService.java
@@ -1,5 +1,6 @@
 package com.back.guest.service;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -27,6 +28,22 @@ public class GuestService {
 
 	public int callmyteam(String id) {
 		return guestDAO.callmyteam(id);
+	}
+
+	public Map<String, GuestDTO> guestModify(int guestIdx) {
+		Map<String, GuestDTO> map = new HashMap<String, GuestDTO>();
+		GuestDTO dto = guestDAO.guestModify(guestIdx);
+		logger.info(dto.getCourt_name()+" ");
+		map.put("guestInfo", dto);
+		return map;
+	}
+
+	public Map<String, Object> writeUpdate(String guest_info, String guest_level, String guest_position,
+			String guest_gender, String guest_to, int guest_fee, String guestIdx) {
+		guestDAO.writeUpdate(guest_info, guest_level, guest_position, guest_gender, guest_to, guest_fee, guestIdx);
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("result", true);
+		return map;
 	}
 	
 }
